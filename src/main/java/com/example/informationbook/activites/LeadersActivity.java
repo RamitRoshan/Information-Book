0b35a0a4b2ff8d02ca5c9db5c7a8ea31fb62f77d
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2024-03-29 18:04:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/informationbook/activites/LeadersActivity.java
@@ -0,0 +1,16 @@
+package com.example.informationbook.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.informationbook.R;
+
+public class LeadersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaders);
+    }
+}
